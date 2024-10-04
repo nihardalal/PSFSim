1f3800490cf22eed533a3e5f0d54cd5a27c334e6
nihardalal <nihardalal@github.com>
2024-10-04 11:29:04 -0400

    Functionality for random star positions added, no longer require positions from catalog

--- makeImage.py
+++ makeImage.py
@@ -8,7 +8,7 @@ def makeParser():
     parser = argparse.ArgumentParser(description="Input Files for PSF Simulation")
     parser.add_argument('wcsFileName', help = 'Pathname to Fits File for WCS')
     parser.add_argument('starCat', help = 'Pathname to Star Catalog')
-
+    parser.add_argument('-r', '--random', dest = "randomPos", action = 'store_true', help = 'Use random positions for stars', default = True)
     return parser
 
 
@@ -19,23 +19,48 @@ if __name__ == '__main__':
 
     #Read RA,Dec from star catalog
     try:
-        assert('.txt' in args.starCat)
+        assert('.fits' in args.starCat)
     except:
-        raise Exception("Star Catalog should be a .txt file")
-    
-    #starCat = np.loadtxt(args['starCat'])
+        raise Exception("Star Catalog should be a .fits file")
 
-    table = Table.read(args.starCat, format = 'ascii')
+    cat = galsim.Catalog(args.starCat)
     
     degrees = galsim.AngleUnit(np.pi/180)
-    ra = table['RAJ2000']*degrees
-    dec = table['DECJ2000']*degrees
+    
+    readImage = galsim.fits.read(file_name = args.wcsFileName, hdu = 1)
+    mybounds = readImage.bounds
+    mywcs = readImage.wcs
 
-    worldPos = [galsim.CelestialCoord(ra = ra[i], dec = dec[i]) for i in range(len(ra))]
+    
 
-    mywcs = galsim.FitsWCS(args.wcsFileName)
+    outImage = galsim.Image(wcs = mywcs, bounds = mybounds)
 
-    pixelPos = [mywcs.posToImage(worldPos[i]) for i in range(len(worldPos))]
+    psf = galsim.Moffat(beta=3, fwhm = 2.85)
+    source = galsim.Convolve([psf, galsim.DeltaFunction(flux=1.)])
 
-    print(pixelPos[0])
+    for i in range(cat.nobjects):
+        if not args.randomPos:
+            ra = cat.get(i,'RAJ2000')*degrees
+            dec = cat.get(i, 'DECJ2000')*degrees
+
+            worldCenter = galsim.CelestialCoord(ra = ra, dec = dec) 
+            imageCenter = mywcs.posToImage(worldCenter)
+        else:
+            x = np.random.random_sample()*mybounds.getXMax()
+            y = np.random.random_sample()*mybounds.getYMax()
+            imageCenter = galsim.PositionD(x = x, y= y)
+
+        nPhot = 30000 #Need to update as a function of SED for a given star 
+
+        psf.drawImage(outImage, method = 'phot', n_photons = nPhot, center = imageCenter, add_to_image = True)
+        print("Image Drawn!")
+
+    outImage.write('outImage.fits')
+    print("Image written to outImage.fits")
+
+
+
+    
+
+
 
