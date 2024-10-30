057c524a5416b70651cb35ce2f915aea336900c8
nihardalal <nihardalal@github.com>
2024-10-30 16:27:12 -0400

    Fixed blackbody code

--- makeImage.py
+++ makeImage.py
@@ -3,15 +3,24 @@ import galsim
 from astropy.table import Table
 import argparse
 import pandas as pd
+from astropy.modeling.physical_models import BlackBody
+from astropy import units as u
+import astropy.io as aio
+from astropy import constants as const
+
+fNuRef = 3.631e-23*(u.W/u.m**2)/u.Hz#W/m^2/Hz
 
 def makeParser():
     parser = argparse.ArgumentParser(description="Input Files for PSF Simulation")
     parser.add_argument('wcsFileName', help = 'Pathname to Fits File for WCS')
     parser.add_argument('starCat', help = 'Pathname to Star Catalog')
-    parser.add_argument('-r', '--random', dest = "randomPos", action = 'store_true', help = 'Use random positions for stars', default = True)
+    parser.add_argument('SCA', help = "SCA Number")
+    parser.add_argument('-r', '--random', dest = "randomPos", action = 'store_true', help = 'Use random positions for stars', default = False)
+    parser.add_argument('-b', '--blackbody', dest ="blackBody", action = 'store_true', help = 'Each star is assumed to be 5000 K blackbody for testing purposes', default = False)
     return parser
 
-
+def sedBB(w, T):
+    return ((8*np.pi*const.h*const.c**2/w**5)*1/(np.exp(const.h*const.c/(w*const.k_B*T))-1)).decompose()
 
 if __name__ == '__main__':
     parser=makeParser()
@@ -31,7 +40,18 @@ if __name__ == '__main__':
     mybounds = readImage.bounds
     mywcs = readImage.wcs
 
-    
+    scaNum = int(args.SCA)
+    if scaNum <10:
+        effAreaTable = aio.ascii.read('Roman_effarea_tables_20240327/Roman_effarea_v8_SCA0{}_20240301.ecsv'.format(scaNum))
+    else:
+        effAreaTable = aio.ascii.read('Roman_effarea_tables_20240327/Roman_effarea_v8_SCA{}_20240301.ecsv'.format(scaNum))
+
+    mirrorDiameter = 2.37*u.m
+
+    geomArea = np.pi*mirrorDiameter**2/4
+    transmissionCurve = effAreaTable['F184']*u.m**2/geomArea
+
+    tExp = 120*u.s
 
     outImage = galsim.Image(wcs = mywcs, bounds = mybounds)
 
@@ -42,6 +62,7 @@ if __name__ == '__main__':
         if not args.randomPos:
             ra = cat.get(i,'RAJ2000')*degrees
             dec = cat.get(i, 'DECJ2000')*degrees
+            mag = cat.get(i, 'H')
 
             worldCenter = galsim.CelestialCoord(ra = ra, dec = dec) 
             imageCenter = mywcs.posToImage(worldCenter)
@@ -49,8 +70,23 @@ if __name__ == '__main__':
             x = np.random.random_sample()*mybounds.getXMax()
             y = np.random.random_sample()*mybounds.getYMax()
             imageCenter = galsim.PositionD(x = x, y= y)
+        
+        if args.blackBody:
+            #bb = BlackBody(temperature = 5000*u.K)
+            wav = np.arange(0.400,2.600, 0.001) * u.um
+            fluxUnnorm = sedBB(wav, 5000*u.K)
+            fLambdaRef = fNuRef * const.c / wav**2
+            norm = 10**(-0.4*mag) * np.trapz(fLambdaRef*transmissionCurve*wav, x = wav)/np.trapz(fluxUnnorm*transmissionCurve*wav, x = wav)
+            flux = norm*fluxUnnorm
+            nPhotQ = np.trapz(flux*effAreaTable['F184']*u.m**2*wav*tExp/(const.h * const.c), x= wav)
+            nPhotQ = nPhotQ.decompose()
+            #nPhotQ = nPhotQ.to(1/(u.s*u.cm**2))
+            nPhot = nPhotQ.value
+            print(nPhot)
+        else:
+            nPhot = 300000 #Need to update as a function of SED for a given star 
 
-        nPhot = 30000 #Need to update as a function of SED for a given star 
+        geomAreaCM = geomArea.to(u.cm**2).value
 
         psf.drawImage(outImage, method = 'phot', n_photons = nPhot, center = imageCenter, add_to_image = True)
         print("Image Drawn!")
